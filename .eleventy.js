d3db82934dfa85487640231a2c0a613a33f30d31
Paolo <ppiron@gmail.com>
2022-02-17 19:15:06 +0000

    Updated config

--- .eleventy.js
+++ .eleventy.js
@@ -4,4 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("posts");
+  return {
+    templateFormats: ["html", "liquid", "njk", "yml"],
+  };
 };
