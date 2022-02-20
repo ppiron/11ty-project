238b749ad13a7aebd57f569e7cfa6156663d88a4
Paolo <ppiron@gmail.com>
2022-02-20 12:03:17 +0000

    Added markdown-it plugin

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,20 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("posts");
+
+  let markdownIt = require("markdown-it");
+  let markdownItFM = require("markdown-it-front-matter");
+  let options = {
+    html: true,
+  };
+  let markdownLib = markdownIt(options).use(markdownItFM, function (fm) {
+    console.log(fm);
+  });
+
+  eleventyConfig.setLibrary("md", markdownLib);
+
   return {
-    templateFormats: ["html", "liquid", "njk", "yml"],
+    templateFormats: ["html", "njk", "yml"],
+    // markdownTemplateEngine: false,
   };
 };
